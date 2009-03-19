31d39538eb90b218301ad865450c47054a01d4cb
bedwards <bedwards@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-03-19 16:57:23 +0000

    Core zenpack changes for having core and enterprise Linux SSH zenpacks contribute to Server/SSH/Linux. Reviewd by JP.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.LinuxMonitor@26143 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/LinuxMonitor/__init__.py
+++ ZenPacks/zenoss/LinuxMonitor/__init__.py
@@ -1,8 +1,46 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2009, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
 
+import logging
 import Globals
 import os.path
+from Products.ZenModel.ZenPack import ZenPackBase
+
+log = logging.getLogger('zen.LinuxMonitor')
 
 skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
 from Products.CMFCore.DirectoryView import registerDirectory
 if os.path.isdir(skinsDir):
     registerDirectory(skinsDir, globals())
+
+class ZenPack(ZenPackBase):
+    
+    def install(self, app):
+        """
+        Set the collector plugins for Server/SSH/Linux.
+        """
+        ZenPackBase.install(self, app)
+        app.dmd.Devices.Server.SSH.Linux.zCollectorPlugins = [
+                'zenoss.cmd.uname',
+                'zenoss.cmd.uname_a',
+                'zenoss.cmd.df',
+                'zenoss.cmd.linux.cpuinfo']
+                
+    def remove(self, app, leaveObjects=False):
+        """
+        Remove the collector plugins.
+        """
+        ZenPackBase.remove(self, app, leaveObjects)
+        if not leaveObjects:
+            app.dmd.Devices.Server.SSH.Linux.zCollectorPlugins = []
+            
