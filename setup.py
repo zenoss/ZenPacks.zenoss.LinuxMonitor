e114436d03a2ce8329b9c3778083881fad039b93
mray <mray@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2009-09-24 15:29:34 +0000

    fixes #5583 reviewed by Chip
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.LinuxMonitor@28649 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.LinuxMonitor"
-VERSION = "1.0.0" # don't change this see tracint ticket 1821
+VERSION = "1.1.0"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
