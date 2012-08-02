9a77f67a733a6d2cc13c900d2acb463072898280
pwarren <pwarren@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-08-02 17:06:15 +0000

    Fixes ZEN-2843. Update cycletime for Linux filesystem template. Reviewed by jpeacock.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.LinuxMonitor@62257 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.LinuxMonitor"
-VERSION = "1.2.0"
+VERSION = "1.3.0"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
