d60cc8e2ad79900d28b919e7c900ed9b84bf70a5
Dave Bouchillon <dbouchillon@zenoss.com>
2016-01-14 15:56:37 -0600

    change version to 2.0.0.  no need to fix cpu alias if we're removing it

--- ZenPacks/zenoss/LinuxMonitor/migrate/RemoveCpuAlias.py
+++ ZenPacks/zenoss/LinuxMonitor/migrate/RemoveCpuAlias.py
@@ -1,21 +1,21 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2015, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
-import Globals
 from Products.ZenModel.ZenPack import ZenPackMigration
 from Products.ZenModel.migrate.Migrate import Version
 
 import logging
 log = logging.getLogger("zen.migrate")
 
-def getSshLinux( dmd ):
+
+def getSshLinux(dmd):
     ssh = None
     sshLinux = None
     if dmd.Devices.Server.hasObject('SSH'):
@@ -24,26 +24,27 @@ def getSshLinux( dmd ):
         sshLinux = ssh.Linux
     return sshLinux
 
-class RemoveCpuAlias( ZenPackMigration ):
+
+class RemoveCpuAlias(ZenPackMigration):
     """
     We are removing the cpu__pct alias on ssCpuIdle since it is now on ssCpuIdlePerCpu instead
     """
 
-    version = Version(1, 3, 0)
+    version = Version(2, 0, 0)
 
     def migrate(self, pack):
         try:
-            log.info( 'Removing ssCpuIdle cpu__pct alias if necessary')
-            sshLinux = getSshLinux( pack.dmd )
+            sshLinux = getSshLinux(pack.dmd)
             if sshLinux:
                 deviceTemplate = sshLinux.rrdTemplates.Device
                 # Get the ssCpuIdle datapoint
                 cpuIdleDp = deviceTemplate.datasources.cpu.datapoints.ssCpuIdle
                 # If it has a cpu__pct alias, delete it
-                if cpuIdleDp.hasAlias( 'cpu__pct' ):
+                if cpuIdleDp.hasAlias('cpu__pct'):
+                    log.info('Removing ssCpuIdle cpu__pct alias')
                     cpuIdleDp.aliases._delObject('cpu__pct')
-        except Exception, e:
-            log.debug( 'Exception trying to remove cpu__pct alias from ssCpuIdle' )
+        except Exception:
+            log.debug('Exception trying to remove cpu__pct alias from ssCpuIdle')
 
 
 RemoveCpuAlias()
