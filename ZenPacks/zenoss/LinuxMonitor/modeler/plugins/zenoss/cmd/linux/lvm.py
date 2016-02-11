1a65f84d20777751304ae9d33474d6ddae7a2615
Dave Bouchillon <dbouchillon@zenoss.com>
2016-02-11 13:39:40 -0600

    update for lvm monitoring and modeling

--- ZenPacks/zenoss/LinuxMonitor/modeler/plugins/zenoss/cmd/linux/lvm.py
+++ ZenPacks/zenoss/LinuxMonitor/modeler/plugins/zenoss/cmd/linux/lvm.py
@@ -19,6 +19,8 @@ import logging
 
 from Products.DataCollector.plugins.CollectorPlugin import CommandPlugin
 from Products.DataCollector.plugins.DataMaps import ObjectMap, RelationshipMap
+
+from ZenPacks.zenoss.LinuxMonitor.util import LVMAttributeParser
 log = logging.getLogger('zen.lvm')
 
 
@@ -56,6 +58,7 @@ class lvm(CommandPlugin):
         pv_maps = []
         lv_maps = []
         sv_maps = []
+        lvm_parser = LVMAttributeParser()
         for line in results.split('\n'):
             columns = line.split()
             if not columns:
@@ -80,7 +83,7 @@ class lvm(CommandPlugin):
                 pv_om.title = columns[0]
                 pv_om.id = self.prepId(columns[0])
                 pv_om.format = columns[1]
-                pv_om.attributes = self.pv_attributes(columns[2])
+                pv_om.attributes = lvm_parser.pv_attributes(columns[2])
                 pv_om.pvsize = int(columns[3])
                 pv_om.free = int(columns[4])
                 pv_om.uuid = columns[5]
@@ -93,7 +96,7 @@ class lvm(CommandPlugin):
                 vg_om = ObjectMap()
                 vg_om.title = columns[0]
                 vg_om.id = self.prepId(columns[0])
-                vg_om.attributes = self.vg_attributes(columns[1])
+                vg_om.attributes = lvm_parser.vg_attributes(columns[1])
                 vg_om.vgsize = int(columns[2])
                 vg_om.freesize = int(columns[3])
                 vg_om.uuid = columns[4]
@@ -105,7 +108,7 @@ class lvm(CommandPlugin):
                 lv_om.title = columns[0]
                 lv_om.vgname = columns[1]
                 lv_om.id = self.prepId(columns[1]+'_'+columns[0])
-                lv_om.attributes = self.lv_attributes(columns[2])
+                lv_om.attributes = lvm_parser.lv_attributes(columns[2])
                 lv_om.lvsize = int(columns[3])
                 lv_om.active = True if 'active' in lv_om.attributes else False
                 lv_om.uuid = columns[4]
@@ -154,158 +157,3 @@ class lvm(CommandPlugin):
                         modname="ZenPacks.zenoss.LinuxMonitor.SnapshotVolume",
                         objmaps=lv_sv_oms))
         return maps
-
-    def pv_attributes(self, atts):
-        # (a)llocatable, e(x)ported and (m)issing
-        attributes = []
-        if atts[0] == 'a':
-            attributes.append('allocatable')
-        if atts[1] == 'x':
-            attributes.append('exported')
-        if atts[2] == 'm':
-            attributes.append('missing')
-        return attributes
-
-    def lv_attributes(self, atts):
-        attributes = []
-        attribute = self.lv_volume_type(atts[0])
-        if attribute:
-            attributes.append(attribute)
-        attribute = self.lv_permissions(atts[1])
-        if attribute:
-            attributes.append(attribute)
-        attribute = self.lv_allocation_policy(atts[2])
-        if attribute:
-            attributes.append(attribute)
-        attribute = self.lv_state(atts[4])
-        if attribute:
-            attributes.append(attribute)
-        attribute = self.lv_device(atts[5])
-        if attribute:
-            attributes.append(attribute)
-        attribute = self.lv_target_type(atts[6])
-        if attribute:
-            attributes.append(attribute)
-        attribute = self.lv_health(atts[8])
-        if attribute:
-            attributes.append(attribute)
-        if atts[9] == 'k':
-            attribute = 'skip activation'
-        if attribute:
-            attributes.append(attribute)
-        return attributes
-
-    def vg_attributes(self, atts):
-        attributes = []
-        attribute = self.vg_permissions(atts[0])
-        if attribute:
-            attributes.append(attribute)
-        if atts[1] == 'z':
-            attributes.append('resizable')
-        if atts[2] == 'x':
-            attributes.append('exported')
-        if atts[3] == 'p':
-            attributes.append('partial')
-        attribute = self.vg_allocation_policy(atts[4])
-        if attribute:
-            attributes.append(attribute)
-        attribute = self.vg_cluster(atts[5])
-        if attribute:
-            attributes.append(attribute)
-        return attributes
-
-    def vg_permissions(self, att):
-        # 1  Permissions: (w)riteable, (r)ead-only
-        return {'w': 'writeable',
-                'r': 'read-only'}.get(att, None)
-
-    def vg_allocation_policy(self, att):
-        #  Allocation policy: (c)ontiguous, c(l)ing, (n)ormal, (a)nywhere
-        return {'c': 'contiguous',
-                'l': 'cling',
-                'n': 'normal',
-                'a': 'anywhere'}.get(att, None)
-
-    def vg_cluster(self, att):
-        # (c)lustered, (s)hared
-        return {'c': 'clustered',
-                's': 'shared'}.get(att, None)
-
-    def lv_volume_type(self, att):
-        # Volume type: (m)irrored, (M)irrored without initial sync, (o)rigin, (O)rigin with merging snapshot,
-        # (r)aid, (R)aid without initial sync, (s)napshot, merging (S)napshot, (p)vmove, (v)irtual, mirror
-        # or raid (i)mage, mirror or raid (I)mage out-of-sync, mirror (l)og device, under (c)onversion,
-        # thin (V)olume, (t)hin pool, (T)hin pool data, raid or thin pool m(e)tadata
-        return {'m': 'mirrored',
-                'M': 'mirrored without initial sync',
-                'o': 'origin',
-                'O': 'origin with merging snapshot',
-                'r': 'raid',
-                'R': 'raid without initial sync',
-                's': 'snapshot',
-                'S': 'merging snapshot',
-                'p': 'pvmove',
-                'v': 'virtual',
-                'i': 'mirror or raid image',
-                'I': 'image out of sync',
-                'l': 'mirror log device',
-                'c': 'under conversion',
-                'V': 'thin volume',
-                't': 'thin pool',
-                'T': 'thin pool data',
-                'e': 'raid or thin pool metadata'}.get(att, None)
-
-    def lv_permissions(self, att):
-        # Permissions: (w)riteable, (r)ead-only, (R)ead-only activation of non-read-only volume
-        return {'w': 'writeable',
-                'r': 'read-only',
-                'R': 'read-only activation of non-read-only volume'}.get(att, None)
-
-    def lv_allocation_policy(self, att):
-        # Allocation policy: (a)nywhere, (c)ontiguous, (i)nherited, c(l)ing, (n)ormal
-        return {'a': 'anywhere',
-                'c': 'contiguous',
-                'i': 'inherited',
-                'l': 'cling',
-                'n': 'normal',
-                'A': 'anywhere (Locked)',
-                'C': 'contiguous (Locked)',
-                'I': 'inherited (Locked)',
-                'L': 'cling (Locked)',
-                'N': 'normal (Locked)'}.get(att, None)
-
-    def lv_state(self, att):
-        # State: (a)ctive, (s)uspended, (I)nvalid snapshot, invalid (S)uspended snapshot,
-        # snapshot (m)erge failed, suspended snapshot (M)erge failed,
-        # mapped (d)evice present without tables, mapped device present with (i)nactive table
-        return {'a': 'active',
-                's': 'suspended',
-                'I': 'invalid snapshot',
-                'S': 'invalid suspended snapshot',
-                'm': 'snapshot merge failed',
-                'M': 'suspended snapshot merge failed',
-                'd': 'mapped device present without tables',
-                'i': 'mapped device present with inactive table'}.get(att, None)
-
-    def lv_device(self, att):
-        # device (o)pen, (X) unknown
-        return {'o': 'open',
-                'X': 'unknown'}.get(att, None)
-
-    def lv_target_type(self, att):
-        # Target  type: (C)ache, (m)irror, (r)aid, (s)napshot, (t)hin, (u)nknown, (v)irtual
-        return {'C': 'cache',
-                'm': 'mirror',
-                'r': 'raid',
-                's': 'snapshot',
-                't': 'thin',
-                'u': 'unknown',
-                'v': 'virtual'}.get(att, None)
-
-    def lv_health(self, att):
-        # Volume Health: (p)artial, (r)efresh needed, (m)ismatches exist, (w)ritemostly, (X) unknown
-        return {'p': 'partial',
-                'r': 'refresh needed',
-                'm': 'mismatches exist',
-                'w': 'writemostly',
-                'X': 'unknown'}.get(att, None)
