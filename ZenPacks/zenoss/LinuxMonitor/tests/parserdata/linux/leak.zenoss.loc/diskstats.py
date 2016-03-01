9ec75a98a592fb17220c7fa79f5e8a99428d2b37
ssaporylo <aporylo@zenoss.com>
2016-03-01 17:25:44 +0200

    fix tests for lvm

--- ZenPacks/zenoss/LinuxMonitor/tests/parserdata/linux/leak.zenoss.loc/diskstats.py
+++ ZenPacks/zenoss/LinuxMonitor/tests/parserdata/linux/leak.zenoss.loc/diskstats.py
@@ -1,45 +1,23 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2016, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
 {
-    'dev_sdb1': dict(readsCompleted=27632,
-                readsMerged=637,
-                sectorsRead=220860,
-                msReading=40333,
-                writesCompleted=38,
-                writesMerged=0,
-                sectorsWritten=4347,
-                msWriting=88,
-                ioInProgress=0,
-                msDoingIO=36191,
-                msDoingIOWeighted=40393),
-    'dev_sda1': dict(readsCompleted=760,
-                readsMerged=0,
-                sectorsRead=41316,
-                msReading=613,
-                writesCompleted=525,
-                writesMerged=0,
-                sectorsWritten=4144,
-                msWriting=80,
-                ioInProgress=0,
-                msDoingIO=401,
-                msDoingIOWeighted=693),
-    'dev_sda2': dict(readsCompleted=31403,
-                readsMerged=691,
-                sectorsRead=457345,
-                msReading=107446,
-                writesCompleted=194965,
-                writesMerged=9003,
-                sectorsWritten=2406372,
-                msWriting=153775,
-                ioInProgress=0,
-                msDoingIO=121519,
-                msDoingIOWeighted=256990),
+    '8:17': dict(readsCompleted=27632,
+                 readsMerged=637,
+                 sectorsRead=220860,
+                 msReading=40333,
+                 writesCompleted=38,
+                 writesMerged=0,
+                 sectorsWritten=4347,
+                 msWriting=88,
+                 ioInProgress=0,
+                 msDoingIO=36191,
+                 msDoingIOWeighted=40393),
 }
