6ab81e4081fa7d82bd67971bed092b889d826971
Chet Luther <chet.luther@gmail.com>
2016-08-02 21:29:34 +0000

    Finish 2.0.3 hotfix: 2.0.3dev -> 2.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.LinuxMonitor"
-VERSION = "2.0.3dev"
+VERSION = "2.0.3"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
