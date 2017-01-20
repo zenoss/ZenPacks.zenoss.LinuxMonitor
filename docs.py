a47bb9f80209c41972f4bb32575e20d0b0e25510
Philip Carinhas <pcarinhas@zenoss.com>
2017-01-20 14:18:24 -0600

    Update documentation for new Catalog
    * Create README.rst as source documentation
    * Add docs.py to create html from rst
    * Add GNUmakefile: "make docs"
    * Remove README.mediawiki
    * Update README.html

---
+++ docs.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import re
+from docutils.core import publish_parts
+
+rx_comments = re.compile(r'/\*.*?\*/\n', re.DOTALL)
+rx_blanks = re.compile(r'\n\n\n')
+
+try:
+    ifd = open('README.rst', 'r')
+    data = ifd.read()
+    ifd.close()
+    parts = publish_parts(data, writer_name="html")
+    html = parts['html_body']
+    stylesheet = parts['stylesheet']
+    stylesheet = rx_comments.sub('', stylesheet)
+    stylesheet = re.sub('\n\n\n', '\n', stylesheet)
+    stylesheet = re.sub('\n\n', '\n', stylesheet)
+
+except Exception:
+    print "Error converting to html"
+
+else:
+    ofd = open('README.html', 'w')
+    ofd.write(stylesheet)
+    ofd.write(html)
+    ofd.close()
