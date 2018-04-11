cc04ce415ede35bb27b901eb74f8b04529f19f19
alam-zenoss <alam@zenoss.com>
2018-04-11 16:22:53 -0500

    Fixes nice cpu for 10+ cpus
    silly replacement

--- ZenPacks/zenoss/LinuxMonitor/parsers/linux/cpu.py
+++ ZenPacks/zenoss/LinuxMonitor/parsers/linux/cpu.py
@@ -48,17 +48,18 @@ class cpu(CommandParser):
             component = getattr(cmd, 'component', None)
 
             # Calculate the number of CPUs
-            cpuCount=0
+            cpuCount = 0
             single_cpu_line = ''
+
             for line in cmd.result.output.splitlines()[1:]:
-              words = line.split()[0:]
-              if 'cpu' in words[0]:
-                cpuCount+=1
-                if component:
-                  if component == line[3]:
-                    # If component, we matched the socket
-                    single_cpu_line = line
-                    break
+                # line e.g. 'cpu0 21306 149059 1785 18404 88442 0 117231 0 0 0'
+                words = line.split()[0:]
+                if 'cpu' in words[0]:
+                    cpuCount += 1
+                    if component and (component == words[0][3:]):
+                        # If component, we matched the socket
+                        single_cpu_line = line
+                        break
 
             # Check if component or device cpu monitoting
             if component:
@@ -68,25 +69,26 @@ class cpu(CommandParser):
             else:
                 # If we got a CPU count, set the normalized perCpu values
                 if cpuCount:
-                  perCpuValues = cmd.result.output.splitlines()[0].split()
-                  userCpuPerCpu = float(perCpuValues[1]) / float(cpuCount)
-                  niceCpuPerCpu = float(perCpuValues[2]) / float(cpuCount)
-                  systemCpuPerCpu = float(perCpuValues[3]) / float(cpuCount)
-                  idleCpuPerCpu = float(perCpuValues[4]) / float(cpuCount)
-                  usedCpuPerCpu = userCpuPerCpu + systemCpuPerCpu
-                  waitCpuPerCpu = float(perCpuValues[5]) / float(cpuCount)
+                    perCpuValues = cmd.result.output.splitlines()[0].split()
+                    userCpuPerCpu = float(perCpuValues[1]) / float(cpuCount)
+                    niceCpuPerCpu = float(perCpuValues[2]) / float(cpuCount)
+                    systemCpuPerCpu = float(perCpuValues[3]) / float(cpuCount)
+                    idleCpuPerCpu = float(perCpuValues[4]) / float(cpuCount)
+                    usedCpuPerCpu = userCpuPerCpu + systemCpuPerCpu
+                    waitCpuPerCpu = float(perCpuValues[5]) / float(cpuCount)
 
             values = perCpuValues[1:]
 
             valueMap = dict(zip(ids, values))
-            valueMap['ssCpuUserPerCpu']=userCpuPerCpu
-            valueMap['ssCpuNicePerCpu']=niceCpuPerCpu
-            valueMap['ssCpuSystemPerCpu']=systemCpuPerCpu
-            valueMap['ssCpuIdlePerCpu']=idleCpuPerCpu
-            valueMap['ssCpuUsedPerCpu']=usedCpuPerCpu
-            valueMap['ssCpuWaitPerCpu']=waitCpuPerCpu
+            valueMap['ssCpuUserPerCpu'] = userCpuPerCpu
+            valueMap['ssCpuNicePerCpu'] = niceCpuPerCpu
+            valueMap['ssCpuSystemPerCpu'] = systemCpuPerCpu
+            valueMap['ssCpuIdlePerCpu'] = idleCpuPerCpu
+            valueMap['ssCpuUsedPerCpu'] = usedCpuPerCpu
+            valueMap['ssCpuWaitPerCpu'] = waitCpuPerCpu
 
             for id in valueMap:
-                if datapointMap.has_key(id):
-                    result.values.append((datapointMap[id], long(valueMap[id])))
+                if id in datapointMap:
+                    result.values.append((datapointMap[id],
+                                          long(valueMap[id])))
         return result
